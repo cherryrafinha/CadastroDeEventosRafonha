32c179bab6a4c3da3584fa147bf0f3988acab948
andre <andre@BOOK-SO793P1I88>
2024-11-16 23:00:55 -0300

    Adicionar arquivos de projeto.

---
+++ CadastroDeEventosRafonha/Views/EventoContratado.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace CadastroDeEventosRafonha.Views;
+
+public partial class EventoContratado : ContentPage
+{
+    public EventoContratado()
+    {
+        InitializeComponent();
+    }
+
+    private async void Button_Clicked(object sender, EventArgs e)
+    {
+        // Voltar para a página anterior
+        await Navigation.PopAsync();
+    }
+}
